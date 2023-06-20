3a980c275b9633eeeb482feb1f08710f36676af3
sergiopaniego <sergiopaniegoblanco@gmail.com>
2023-06-20 14:46:04 +0200

    Removed commented code

--- Carla-FollowLane/pytorch/PilotNet/tensorrt_optimize_int8.py
+++ Carla-FollowLane/pytorch/PilotNet/tensorrt_optimize_int8.py
@@ -5,6 +5,7 @@ import torch
 import torch_tensorrt
 import torchvision
 import pytorch_quantization
+import argparse
 
 import numpy as np
 import torch.nn as nn
@@ -249,10 +250,8 @@ def evaluate_model(model_path, opt_model, val_set, val_loader):
     return model_size, mse, inf_time
 
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
-
 
     parser.add_argument("--data_dir", action='append', help="Directory to find Train Data")
     parser.add_argument("--preprocess", action='append', default=None, help="preprocessing information: choose from crop/nocrop and normal/extreme")
@@ -268,22 +267,6 @@ def parse_args():
     parser.add_argument("--val_split", type=float, default=0.2, help="Train test Split")
     parser.add_argument("--model_name", type=str, default="PilotNet", help="Model name")
 
-    '''
-    parser.add_argument("--data_dir", action='append', help="Directory to find Train Data")
-    parser.add_argument("--test_dir", action='append', help="Directory to find Test Data")
-    parser.add_argument("--preprocess", action='append', default=None, help="preprocessing information: choose from crop/nocrop and normal/extreme")
-    parser.add_argument("--base_dir", type=str, default='exp_random', help="Directory to save everything")
-    parser.add_argument("--comment", type=str, default='Random Experiment', help="Comment to know the experiment")
-    parser.add_argument("--data_augs", action='append', type=str, default=None, help="Data Augmentations")
-    parser.add_argument("--num_epochs", type=int, default=100, help="Number of Epochs")
-    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate for Policy Net")
-    parser.add_argument("--val_split", type=float, default=0.2, help="Train test Split")
-    parser.add_argument("--shuffle", type=bool, default=False, help="Shuffle dataset")
-    parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
-    parser.add_argument("--save_iter", type=int, default=50, help="Iterations to save the model")
-    parser.add_argument("--print_terminal", type=bool, default=False, help="Print progress in terminal")
-    parser.add_argument("--seed", type=int, default=123, help="Seed for reproducing")
-    '''
     args = parser.parse_args()
     return args
 
@@ -291,11 +274,8 @@ def parse_args():
 if __name__=="__main__":
     args = parse_args()
 
-    #image_shape = np.array([200,66, 3])
-    image_shape = np.array([args.input_shape])
-    #device = 'cuda'
+    image_shape = np.array(tuple(map(int, args.input_shape.split(','))))
     device = args.device
-    #model_dir = '/docker-tensorrt/pilot_net_model_best_123.pth'
     model_dir = args.model_dir
     quant_modules.initialize()
 
@@ -303,35 +283,16 @@ if __name__=="__main__":
     pilotModel.load_state_dict(torch.load(model_dir))
 
     augmentations = args.data_augs
-    #augmentations = 'all'
-    #augmentations = ''
-
-    #path_to_data = [
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_31_10_anticlockwise_town_01_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_31_10_clockwise_town_01_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_clockwise_town_01_previous_v_extreme/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_clockwise_town_01_previous_v_extreme/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_anticlockwise_town_03_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_clockwise_town_03_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_anticlockwise_town_05_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_clockwise_town_05_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_anticlockwise_town_07_previous_v/',
-    #    '/docker-tensorrt/carla_dataset_previous_v/carla_dataset_test_04_11_clockwise_town_07_previous_v/',  
-    #    ]
     path_to_data = args.data_dir
     val_split = args.val_split
     #val_split = 0.3
     shuffle_dataset = args.shuffle
-    #shuffle_dataset = True 
     random_seed = args.seed
-    #random_seed = 123
     batch_size = args.batch_size
-    #batch_size = 1
 
     # Define data transformations
     transformations = createTransform(augmentations)
     # Load data
-    #dataset = PilotNetDatasetTest(path_to_data, transformations, preprocessing=['extreme'])
     dataset = PilotNetDatasetTest(path_to_data, transformations, preprocessing=args.preprocess)
 
     # Creating data indices for training and validation splits:
@@ -345,24 +306,20 @@ if __name__=="__main__":
 
     # Creating PT data samplers and loaders:
     test_sampler = SubsetRandomSampler(val_split)
-    testing_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    testing_dataloader = DataLoader(dataset, batch_size=batch_size, sampler=test_sampler)
 
     #Calibrate the model using max calibration technique.
     with torch.no_grad():
         calibrate_model(
             model=pilotModel,
-            #model_name="pilotNet",
             model_name=args.model_name,
             data_loader=testing_dataloader,
-            #num_calib_batch=32,
-            #num_calib_batch=1,
             num_calib_batch=args.batch_size,
             calibrator="max",
             hist_percentile=[99.9, 99.99, 99.999, 99.9999],
             out_dir="./")
 
     # Declare Learning rate
-    #lr = 0.0001
     lr = args.lr
     state = {}
     state["lr"] = lr
@@ -372,7 +329,6 @@ if __name__=="__main__":
 
     # Finetune the QAT model for 1 epoch
     num_epochs=args.num_epochs
-    #num_epochs=10
     for epoch in range(num_epochs):
         adjust_lr(opt, epoch)
         print('Epoch: [%5d / %5d] LR: %f' % (epoch + 1, num_epochs, state["lr"]))
@@ -412,7 +368,6 @@ if __name__=="__main__":
 
 
     benchmark(jit_model, input_shape=(batch_size, args.input_shape[2], args.input_shape[0], args.input_shape[1]))
-    #benchmark(jit_model, input_shape=(16, 3, 200, 66))
 
     model_size, mse, inf_time = evaluate_model('trained_pilotNet_qat.jit.pt', trt_mod, dataset, testing_dataloader)
 
